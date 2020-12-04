d849423a0ceca53737836c01fd9bd68994bf6385
Elvis Retiz <elvis.retiz@hotmail.com>
2020-12-04 11:35:32 -0600

    Modificacion de endpoint payroll y se agrega endpoint para validar con mensaje alive

--- src/controllers/payroll.controller.js
+++ src/controllers/payroll.controller.js
@@ -101,6 +101,81 @@ const controller = {
       })
       
     }
+  },
+  getPayrollsByYear: async (req, res) => {
+    try {
+      const { year, employee } = req.params;
+      let payrollsOfYear = [];
+  
+      const payrolls = await Payroll.findAll({
+        attributes: [
+          'PE_NUMERO',
+          'PE_TIPO',
+          'NO_PERCEPC',
+          'NO_DEDUCCI',
+          'NO_NETO'
+        ],
+        where: {
+          NO_TIMBRO: 2, //status timbrada
+          PE_YEAR: parseInt(year,10),
+          CB_CODIGO: parseInt(employee,10)
+        },
+        logging: () => console.log(chalk.green("Successful query to payrolls"))
+      });
+  
+      const periods = await Period.findAll({
+        attributes: [
+          'PE_NUMERO',
+          'PE_TIPO',
+          'PE_FEC_PAG'
+        ],
+        where: {
+          PE_YEAR: parseInt(year,10),
+          PE_TIPO: payrolls[0].getDataValue('PE_TIPO')
+        },
+        logging: () => console.log(chalk.green("Successful query to periods"))
+      });
+  
+      for (const payroll of payrolls) {
+  
+        let payrollObject = {};
+  
+        payrollObject.PERNR = `${employee}`;
+        payrollObject.CCODE = config.companyCode;
+  
+        let periodOfPayroll = periods.find((period) => (
+          period.getDataValue('PE_TIPO') === payroll.getDataValue('PE_TIPO') 
+          &&
+          period.getDataValue('PE_NUMERO') === payroll.getDataValue('PE_NUMERO')
+        ));
+  
+        //remove id is necesary
+        let date = new Date(periodOfPayroll.getDataValue('PE_FEC_PAG'))
+        date.setDate(date.getDate()+1);
+  
+        payrollObject.PAYDAY = dayjs(date).format('DD/MM/YYYY');
+        payrollObject.PLUS = payroll.getDataValue('NO_PERCEPC');
+        payrollObject.MINUS = payroll.getDataValue('NO_DEDUCCI');
+        payrollObject.TOTAL = payroll.getDataValue('NO_NETO');
+  
+        payrollsOfYear.push(payrollObject);
+  
+      }
+  
+      return res.send({
+        PAYMENTList: payrollsOfYear,
+        IF_RESULT: "S",
+        IF_MESSAGE: ""
+      });
+    } catch (error) {
+      
+      return res.send({
+        PAYMENTList: [],
+        IF_RESULT: "E",
+        IF_MESSAGE: "No information was found with the specified parameters."
+      });
+
+    }
   }
 };
 
